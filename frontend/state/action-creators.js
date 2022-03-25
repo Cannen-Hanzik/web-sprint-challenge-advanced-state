52060235db8f58a50c08f97cc679fc7661981e6e
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-03-25 11:56:01 -0500

    finished setting up ASYNC action creators

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -39,24 +39,42 @@ export function resetForm() {
 
 export function fetchQuiz() {
   return function (dispatch) {
-    // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
-    // On successful GET:
-    // - Dispatch an action to send the obtained quiz to its state
-  }
+    axios
+      .get(`http://localhost:9000/api/quiz/next`)
+      .then((res) => {
+        dispatch(setQuiz(res.data));
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+  };
 }
-export function postAnswer() {
+export function postAnswer({ quiz_id, answer_id }) {
   return function (dispatch) {
-    // On successful POST:
-    // - Dispatch an action to reset the selected answer state
-    // - Dispatch an action to set the server message to state
-    // - Dispatch the fetching of the next quiz
-  }
+    axios
+      .post(`http://localhost:9000/api/quiz/answer`, { quiz_id, answer_id })
+      .then((res) => {
+        dispatch(fetchQuiz());
+        dispatch(setMessage(res.data.message));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 }
-export function postQuiz() {
+export function postQuiz({ question_text, true_answer_text, false_answer_text }) {
   return function (dispatch) {
-    // On successful POST:
-    // - Dispatch the correct message to the the appropriate state
-    // - Dispatch the resetting of the form
-  }
+    axios
+      .post(`http://localhost:9000/api/quiz/new`, { question_text, true_answer_text, false_answer_text })
+      .then((res) => {
+        dispatch(
+        setMessage(`Fantastic: "${res.data.question}" is a fantastic one!`)
+        );
+        dispatch(resetForm);
+      })
+      .catch((err) => {
+        console.error(err)
+      });
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
