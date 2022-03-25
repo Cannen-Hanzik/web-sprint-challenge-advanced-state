8c23eee66ea2f838002588a90f84a97133f3910a
Cannen Hanzik <cannenhanzik2@gmail.com>
2022-03-25 12:47:07 -0500

    figured out what was wrong with the quiz submission

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -62,18 +62,19 @@ export function postAnswer({ quiz_id, answer_id }) {
       });
   };
 }
-export function postQuiz({ question_text, true_answer_text, false_answer_text }) {
+export function postQuiz({question_text,true_answer_text,false_answer_text}) {
   return function (dispatch) {
     axios
-      .post(`http://localhost:9000/api/quiz/new`, { question_text, true_answer_text, false_answer_text })
+      .post(`http://localhost:9000/api/quiz/new`, {question_text,true_answer_text,false_answer_text,
+      })
       .then((res) => {
         dispatch(
-        setMessage(`Fantastic: "${res.data.question}" is a fantastic one!`)
+          setMessage(`Congrats: "${res.data.question}" is a great question!`)
         );
-        dispatch(resetForm);
+        dispatch(resetForm());
       })
       .catch((err) => {
-        console.error(err)
+        console.error(err);
       });
   };
 }
